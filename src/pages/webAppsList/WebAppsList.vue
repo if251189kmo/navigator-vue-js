8c9c8fd10000d995ffea5acba2782953ed0aa9ff
if251189KMO <if251189KMO@gmail.com>
2025-09-19 14:42:11 +0300

    fix tab border

--- src/pages/webAppsList/WebAppsList.vue
+++ src/pages/webAppsList/WebAppsList.vue
@@ -1,21 +1,39 @@
 <template>
   <q-intersection transition="scale">
-    <div :class="$style.webAppsList">webAppsList</div>
-    <div :class="$style.webAppsList">webAppsList</div>
-    <div :class="$style.webAppsList">webAppsList</div>
-    <div :class="$style.webAppsList">webAppsList</div>
-    <div :class="$style.webAppsList">webAppsList</div>
-    <div :class="$style.webAppsList">webAppsList</div>
+    <ul :class="$style.webAppsList" v-for="link in getLinks" :key="link.id">
+      <li>
+        <div>{{ link.id }}</div>
+        <div>{{ link.code }}</div>
+        <div>{{ link.label }}</div>
+        <div>{{ link.new }}</div>
+      </li>
+    </ul>
     <q-btn to="/" style="background: #ff0080; color: white" label="go home" />
   </q-intersection>
 </template>
 
-<!-- <script setup lang="ts">
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
+import Progresses from 'src/constants/progresses'
+import { useHomeStore } from 'src/stores/home'
 
-</script> -->
+const homeStore = useHomeStore()
+const { getLinks } = storeToRefs(homeStore)
+
+void homeStore.fetchLinks({ progressName: Progresses.PROGRESS_PAGE })
+</script>
 
 <style module lang="scss">
 .webAppsList {
   background-color: rgba(233, 40, 217, 0.733);
+
+  li {
+    display: flex;
+    align-items: center;
+
+    div {
+      margin: 5px;
+    }
+  }
 }
 </style>
